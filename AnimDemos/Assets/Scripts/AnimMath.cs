eed041fb32bf98573ebd7a66c181741bae7eb4a4
Eric Versaw <ericversaw22@gmail.com>
2021-02-11 18:30:45 -0500

    week 5
    added quaternions to animMath class

--- AnimDemos/Assets/Scripts/AnimMath.cs
+++ AnimDemos/Assets/Scripts/AnimMath.cs
@@ -13,7 +13,6 @@ public static class AnimMath
         }
         return (max - min) * p + min;
     }
-
     public static Vector3 Lerp(Vector3 min, Vector3 max, float p, bool allowEx = true)
     {
         if (!allowEx)
@@ -22,6 +21,16 @@ public static class AnimMath
             if (p > 1) p = 1;
         }
         return (max - min) * p + min;
+    }
+    public static Quaternion Lerp(Quaternion min, Quaternion max, float p, bool allowExtra = true)
+    {
+        Quaternion rot = Quaternion.identity;
+        rot.x = Lerp(min.x, max.x, p, allowExtra);
+        rot.y = Lerp(min.y, max.y, p, allowExtra);
+        rot.z = Lerp(min.z, max.z, p, allowExtra);
+        rot.w = Lerp(min.w, max.w, p, allowExtra);
+
+        return rot;
     }
 
     public static float Slide(float curr, float target, float percentLeftAfter1)
@@ -34,6 +43,12 @@ public static class AnimMath
         float p = 1 - Mathf.Pow(percentLeftAfter1, Time.deltaTime);
         return AnimMath.Lerp(curr, target, p);
     }
+    public static Quaternion Slide(Quaternion current, Quaternion target, float percentLeft = 0.5f)
+    {
+        float p = 1 - Mathf.Pow(percentLeft, Time.deltaTime);
+        return AnimMath.Lerp(current, target, p);
+    }
+
 
     public static Vector3 SpotOnCircleXZ(float radius, float currentAngle)
     {
--- AnimDemos/Assets/Scripts/SlideTowards.cs
+++ AnimDemos/Assets/Scripts/SlideTowards.cs
@@ -6,8 +6,25 @@ public class SlideTowards : MonoBehaviour
 {
     public Transform target;
 
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = GetComponentInChildren<Camera>();
+    }
     void Update()
     {
+        //slides postion
         transform.position = AnimMath.Slide(transform.position, target.position, .01f);
+
+        //get vec to target
+        Vector3 vectorToTarget = target.position - cam.transform.position;
+
+        //create desired rotation
+        Quaternion targetRotation = Quaternion.LookRotation(vectorToTarget, Vector3.up);
+
+        //set camera to ease
+        cam.transform.rotation = AnimMath.Slide(cam.transform.rotation, targetRotation, .05f);
+
     }
 }
